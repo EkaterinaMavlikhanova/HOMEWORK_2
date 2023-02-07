bbdec1f688f89a44fc5dbb20beb4a815961497df
Katerina Mavlikhanova <madam.andreewna2016@yandex.ru>
2023-02-07 14:09:41 +0300

    add new info

---
+++ homework001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, котроая принимает на вход трехзначное число, а на выходе показывает вторую цифру этого числа.
+// 536->"3"
+// 248->"4"
+// 654->"5"
+
+int GetNumber ()
+{
+    Random rnd = new Random ();
+    int result = rnd.Next(100,1000);
+    return result;
+}
+
+int GetSecondOfNumber (int number)
+{
+    int firstDigit = number / 100;
+    int secondDigit = number % 10;
+    return number / 10 % 10;
+}
+
+int number = GetNumber ();
+int result = GetSecondOfNumber (number);
+
+Console.WriteLine($"Было:{number}, Стало: {result}");
