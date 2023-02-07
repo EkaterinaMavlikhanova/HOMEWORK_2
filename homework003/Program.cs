5e440777df85fd248590ebc99b695a9ec66a3c5a
Katerina Mavlikhanova <madam.andreewna2016@yandex.ru>
2023-02-07 14:16:07 +0300

    add new info to homework003

---
+++ homework003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int NumberRequest (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int WorkHoliday (int number)
+{
+ if (number > 0 && number < 8)
+    {
+        if (number == 7 || number == 6)
+        {
+            Console.Write("Под цифрой " + number + " - Выходной");
+        }
+        else
+        {
+            Console.Write("Под цифрой " + number + " - Рабочий");
+        }
+    }
+ else
+    {
+        Console.Write("Вы ввели число не в пределах от 1 до 7, не возможно определить");
+    }
+}
